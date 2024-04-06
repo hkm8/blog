1067509de8206fd1d43892c2979f469018565796
hkm8 <adam.mikut@gmail.com>
2024-04-06 15:30:08 +0200

    fix

--- src/main/java/com/blog/blog/blogPostRepository.java
+++ src/main/java/com/blog/blog/blogPostRepository.java
@@ -2,6 +2,6 @@ package com.blog.blog;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<blogPost, Long> {
+public interface blogPostRepository extends JpaRepository<blogPost, Long> {
     // You can add custom query methods here if needed
 }
